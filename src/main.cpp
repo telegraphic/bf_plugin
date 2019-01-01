21fa919edde45f9d7b15af6081f820c3c4232a11
Danny Price <dan@thetelegraphic.com>
2019-01-01 16:51:32 +0800

    Added 16 bit to 8 bit converter

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,30 @@ double compute_stdev(py::array_t<double> input) {
 
 }
 
-void requant_8i_2u(py::array_t<char> input, py::array_t<unsigned char> output) {
+void requant_i16_i8(py::array_t<int16_t> input, py::array_t<int8_t> output, float scale_factor) {
+    // Setup pointers and buffers to access data in ndarray
+    py::buffer_info buf = input.request();
+    py::buffer_info bufo = output.request();
+    auto *v   = (int16_t *) buf.ptr;
+    auto *vo  = (int8_t *) bufo.ptr;
+
+    int8_t scaled_max = 127;
+    int8_t scaled_min = -127;
+    size_t N = buf.shape[0];
+    for(size_t idx = 0; idx < N; idx++){
+        int16_t scaled_val = v[idx] * scale_factor;
+        if(std::abs(scaled_val) <= 127) {
+            vo[idx] = scaled_val;
+        }
+        else if (scaled_val <= -128) {
+            vo[idx] = scaled_min;
+        } else {
+            vo[idx] = scaled_max;
+        }
+    }
+}
+
+void requant_i8_u2(py::array_t<char> input, py::array_t<unsigned char> output) {
     // Input array should have size 4x that of output array.
     // Output array has to be uint8
 
@@ -110,21 +133,22 @@ void requant_8i_2u(py::array_t<char> input, py::array_t<unsigned char> output) {
 
 PYBIND11_MODULE(requant_utils, m) {
     m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
+        Requantization utilities
+        ------------------------
 
-        .. currentmodule:: python_example
+        .. currentmodule:: requant_utils
 
         .. autosummary::
            :toctree: _generate
 
-           add
-           subtract
+           compute_stdev
+           requant_i8_u2
+           requant_i16_to_i8
     )pbdoc";
 
     m.def("compute_stdev", &compute_stdev, "Compute STDEV");
-
-    m.def("requant_8i_2u", &requant_8i_2u, "Requantize 8bit signed integers [-128, 127] to 2-bit unsigned");
+    m.def("requant_i8_u2", &requant_i8_u2, "Requantize 8bit signed integers [-128, 127] to 2-bit unsigned");
+    m.def("requant_i16_i8", &requant_i16_i8, "Convert 16 bit data to 8 bit data, using scaling factor");
 
 #ifdef VERSION_INFO
     m.attr("__version__") = VERSION_INFO;
